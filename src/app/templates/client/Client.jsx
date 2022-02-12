abe35beecf49e4bbfe23227a83005d942560bc6e
ginnyTheCat <ginnythecat@lelux.net>
2022-02-12 10:05:09 +0100

    Basic key verification support

--- src/app/templates/client/Client.jsx
+++ src/app/templates/client/Client.jsx
@@ -9,12 +9,13 @@ import Room from '../../organisms/room/Room';
 import Windows from '../../organisms/pw/Windows';
 import Dialogs from '../../organisms/pw/Dialogs';
 import EmojiBoardOpener from '../../organisms/emoji-board/EmojiBoardOpener';
+import KeyVerificationOpener from '../../organisms/key-verification/KeyVerificationOpener';
+import DragDrop from '../../organisms/drag-drop/DragDrop';
 import logout from '../../../client/action/logout';
 
 import initMatrix from '../../../client/initMatrix';
 import navigation from '../../../client/state/navigation';
 import cons from '../../../client/state/cons';
-import DragDrop from '../../organisms/drag-drop/DragDrop';
 
 function Client() {
   const [isLoading, changeLoading] = useState(true);
@@ -132,6 +133,7 @@ function Client() {
       <Windows />
       <Dialogs />
       <EmojiBoardOpener />
+      <KeyVerificationOpener />
       <ReusableContextMenu />
       <DragDrop isOpen={dragCounter !== 0} />
     </div>
